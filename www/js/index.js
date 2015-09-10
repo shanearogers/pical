fbb98861531a72a1e2bcc700fa27cdd4c3c01ea5
Shane Rogers <sharoger@gmail.com>
2015-09-10 12:25:07 +1000

    trying mbppower plugin

--- www/js/index.js
+++ www/js/index.js
@@ -17,47 +17,67 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.getElementById('startCameraButton').addEventListener('mousedown', this.onStartCamera, false);
+		document.getElementById('startCameraAnotherPosButton').addEventListener('mousedown', this.onStartCameraAnotherPos, false);
+		
+		document.getElementById('stopCameraButton').addEventListener('mousedown', this.onStopCamera, false);
+		document.getElementById('takePictureButton').addEventListener('mousedown', this.onTakePicture, false);
+		document.getElementById('switchCameraButton').addEventListener('mousedown', this.onSwitchCamera, false);
+		document.getElementById('showButton').addEventListener('mousedown', this.onShow, false);
+		document.getElementById('hideButton').addEventListener('mousedown', this.onHide, false);
+		document.getElementById('colorEffectCombo').addEventListener('change', this.onColorEffectChanged, false);
+		//window.addEventListener('orientationchange', this.onStopCamera, false);
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	onStartCamera: function() {
+		var tapEnabled = true;
+		var dragEnabled = true;
+        var toBack = true;
+		cordova.plugins.camerapreview.startCamera({x: 0, y: 50, width: 300, height:300}, "front", tapEnabled, dragEnabled, toBack);
+	},
+	onStartCameraAnotherPos: function() {
+		var tapEnabled = true;
+		var dragEnabled = true;
+        var toBack = false;
+		cordova.plugins.camerapreview.startCamera({x: 200, y: 0, width: 100, height:150}, "front", tapEnabled, dragEnabled, toBack);
+	},
+	onStopCamera: function() {
+		cordova.plugins.camerapreview.stopCamera();
+	},
+	onTakePicture: function() {
+        cordova.plugins.camerapreview.takePicture({maxWidth:640, maxHeight:640});
+	},
+	onSwitchCamera: function() {
+		cordova.plugins.camerapreview.switchCamera();
+	},
+	onShow: function() {
+		cordova.plugins.camerapreview.show();
+	},
+    onHide: function() {
+        cordova.plugins.camerapreview.hide();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        canvasMain = document.getElementById("camera");
-        CanvasCamera.initialize(canvasMain);
-        // define options
-        var opt = {
-            quality: 75,
-            destinationType: CanvasCamera.DestinationType.DATA_URL,
-            encodingType: CanvasCamera.EncodingType.JPEG,
-            saveToPhotoAlbum:true,
-            correctOrientation:true,
-            width:640,
-            height:480
-        };
-        CanvasCamera.start(opt);
-
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+	onColorEffectChanged: function() {
+		var effect = document.getElementById('colorEffectCombo').value;
+		cordova.plugins.camerapreview.setColorEffect(effect);
+	},
+	
+	// deviceready Event Handler   
+	onDeviceReady: function() {	
+		//on picture
+		cordova.plugins.camerapreview.setOnPictureTakenHandler(function(result){
+			document.getElementById('originalPicture').src = result[0];//originalPicturePath;
+			document.getElementById('previewPicture').src = result[1];//previewPicturePath;
+		});
+	}
 };
+
+app.initialize();
